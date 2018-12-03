113a7b1751b35ee06731408bdcb21d8780488147
abood <tahooo57@gmail.com>
2018-12-03 21:28:15 +0300

    change client file to client server

---
+++ client server/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <a href="/api/ping"> A Server Test</a>
+  </div>
+    , document.getElementById('root'));
